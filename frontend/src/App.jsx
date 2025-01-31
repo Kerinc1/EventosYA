321c8814f31d226e9e88de35a3f9d208cf4daf60
Jhon Acosta <jhon.acosta2006@hotmail.com>
2025-01-31 09:23:19 -0500

    Solucionar error de importación y actualizar proyecto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext'; // Importar el AuthProvider
-import NavigationBar from './components/Navbar.jsx';
+import NavigationBar from './components/Nav.jsx';
 import LoginForm from './components/LoginForm.jsx';
 import RegisterForm from './components/RegisterForm.jsx';
 import Home from './components/Home.jsx'; 
 import Events from './components/Events.jsx'; 
 import EventAdmin from './components/EventAdmin';
 import EventDetails from './components/EventDetails';
-import About from './components/About';
+import About from './components/about';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
